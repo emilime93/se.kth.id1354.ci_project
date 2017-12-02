ca5a61467200f6b1f81a82af2536a7c90fb9faf6
emilime93 <emil93@gmail.com>
2017-12-02 16:32:26 +0100

    Changed the base url to be localhost again after some testing

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-#$config['base_url'] = 'http://localhost';
-$config['base_url'] = 'http://25.65.171.67';
+$config['base_url'] = 'http://localhost';
+#$config['base_url'] = 'http://25.65.171.67';
 //TODO Change this if https
 
 /*
